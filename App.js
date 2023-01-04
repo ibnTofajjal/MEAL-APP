962d400e3396eb00781569cf6129e355e52784ed
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2023-01-04 20:22:10 +0600

    Navigator added

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
+// NAvitegation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Screens
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <View style={styles.container}>
-        <CategoriesScreen />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
